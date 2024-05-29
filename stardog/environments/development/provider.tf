4697b553c04d555e5c62815ff382980956c4db6a
Ky Tran <trantheky1987@gmail.com>
2024-05-29 23:21:24 +0700

    Updated

--- stardog/environments/development/provider.tf
+++ stardog/environments/development/provider.tf
@@ -1,14 +1,14 @@
 # More information https://github.com/hashicorp/terraform-provider-azurerm
 
 # 1. Specify the version of the AzureRM Provider to use
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">=3.0.0"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    azurerm = {
+#      source = "hashicorp/azurerm"
+#      version = ">=3.0.0"
+#    }
+#  }
+#}
 
 # 2. Configure the AzureRM Provider
 provider "azurerm" {
