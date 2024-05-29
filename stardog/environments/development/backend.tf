533ab997f1d11e76ed9436f8ee15a265c1b24c86
Ky Tran <trantheky1987@gmail.com>
2024-05-29 21:47:47 +0700

    Updated

--- stardog/environments/development/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "rg-tf-state-learning"
-        storage_account_name = "storagetfstatelearning"
-        container_name       = "tfstatelearning"
-        key                  = "tflearning.tfstate"
-    }
-}
