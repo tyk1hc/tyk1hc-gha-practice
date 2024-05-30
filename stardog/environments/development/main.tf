75448ae457bdee856aa9141749183a59327d3f30
trantheky <trantheky1987@gmail.com>
2024-05-30 15:33:30 +0700

    updated

--- stardog/environments/development/main.tf
+++ stardog/environments/development/main.tf
@@ -1,7 +1,9 @@
 resource "azurerm_resource_group" "maintest" {
   name     = var.resource_group_name
   location = var.resource_group_location
-  tags = var.resource_environment_tags["development"]
+  tags = {
+    Environment = var.resource_environment_tags["development"]
+  }
 
 
   lifecycle {
