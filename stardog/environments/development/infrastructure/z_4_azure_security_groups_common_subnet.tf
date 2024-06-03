990e5008713fdf1b80dfb05ea225a1493247091a
Ky Tran <trantheky1987@gmail.com>
2024-06-03 11:57:06 +0700

    Updated

--- stardog/environments/development/infrastructure/z_4_azure_security_groups_common_subnet.tf
+++ stardog/environments/development/infrastructure/z_4_azure_security_groups_common_subnet.tf
@@ -12,13 +12,13 @@ resource "azurerm_network_security_rule" "nsg-rule-BTIAAcess" {
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "*"
-  source_address_prefix       = ["103.4.127.176","194.39.218.13"]
+  source_address_prefix       = "103.4.127.176,194.39.218.13"
   destination_address_prefix  = "*"
   resource_group_name         = local.ResourceGroup_Default_Name
   network_security_group_name = azurerm_network_security_group.nsg-common-subnet.name
 }
 
 resource "azurerm_subnet_network_security_group_association" "example" {
-  subnet_id                 = module.vnet.subnets_map[local.Subnet_Common]
+  subnet_id                 = module.azure_virtual_network.subnets_map[local.Subnet_Common]
   network_security_group_id = azurerm_network_security_group.nsg-common-subnet.id
 }
