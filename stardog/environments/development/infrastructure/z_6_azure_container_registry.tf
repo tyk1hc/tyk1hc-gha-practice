86206b3e4702f56e8d01326155b0234bc6c30721
Ky Tran <trantheky1987@gmail.com>
2024-06-04 10:54:35 +0700

    Updated

--- stardog/environments/development/infrastructure/z_6_azure_container_registry.tf
+++ stardog/environments/development/infrastructure/z_6_azure_container_registry.tf
@@ -1,6 +1,6 @@
 # Conainer Registry
 resource "azurerm_container_registry" "container-registry-stardog" {
-  name                = "${local.Project}-${var.AzureResourceTypes["ContainerRegistry"]}-stardog-${local.Environment}-${local.Location}"
+  name                = "${local.Project}${var.AzureResourceTypes["ContainerRegistry"]}stardog${local.Environment}${local.Location}"
   resource_group_name = azurerm_resource_group.resource-group-virtual-network.name
   location            = local.Location
   sku                 = "Standard"
