afef8558a43d47324ce8a7cc85d2bcbe1a44ebb6
trantheky <trantheky1987@gmail.com>
2024-06-05 15:52:45 +0700

    Updated

--- stardog/environments/development/infrastructure/z_9_azure_dns.tf
+++ stardog/environments/development/infrastructure/z_9_azure_dns.tf
@@ -1,6 +1,6 @@
 module "private_dns_aks_customdns_vnet" {
   source                                     = "../../../../modules/terraform.azurerm.module.private-dns"
-  dns_zone_name                              = "${local.Project}-${var.AzureResourceTypes["PrivateDomainNameServer"]}-aks-${local.Environment}"
+  dns_zone_name                              = "${local.Project}-${var.AzureResourceTypes["PrivateDomainNameServer"]}-${local.Environment}.aks.io"
   resource_group_name                        = azurerm_resource_group.resource-group-virtual-network.name
   location                                   = local.Location
   private_dns_zone_virtual_network_link_name = "${local.Project}-${var.AzureResourceTypes["PrivateLinkDomainNameServer"]}-aks-${local.Environment}"
