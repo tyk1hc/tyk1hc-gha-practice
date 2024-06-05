f10759bcc55c835cffc5bb07744ba9ef1bcc7658
trantheky <trantheky1987@gmail.com>
2024-06-05 13:33:13 +0700

    Updated

--- stardog/environments/development/infrastructure/z_7_azure_storage_accounts.tf
+++ stardog/environments/development/infrastructure/z_7_azure_storage_accounts.tf
@@ -1,17 +1,39 @@
-module "diagstore" {
-  source                       = "../../../../modules/terraform.azurerm.module.storageaccount"
-  name                         = "${local.Project}${var.AzureResourceTypes["StorageAccount"]}diagstore${local.Environment}"
-  resource_group_name          = azurerm_resource_group.resource-group-virtual-network.name
-  location                     = local.Location
-  tags                         = { Environment = "Dev" }
-  account_tier                 = "Standard"
-  account_kind                 = "StorageV2"
-  account_replication_type     = "LRS"
-  network_rules_default_action = "Deny"
-  network_rules_bypass         = ["AzureServices", "Logging"]
-  min_tls_version              = "TLS1_2"
-  log_analytics_workspace_id   = azurerm_log_analytics_workspace.Log_Analytics_Workspace_Default.id
-  advanced_threat_protection   = "false"
+#module "diagstore" {
+#  source                       = "../../../../modules/terraform.azurerm.module.storageaccount"
+#  name                         = "${local.Project}${var.AzureResourceTypes["StorageAccount"]}diagstore${local.Environment}"
+#  resource_group_name          = azurerm_resource_group.resource-group-virtual-network.name
+#  location                     = local.Location
+#  tags                         = { Environment = "Dev" }
+#  account_tier                 = "Standard"
+#  account_kind                 = "StorageV2"
+#  account_replication_type     = "LRS"
+#  network_rules_default_action = "Deny"
+#  network_rules_bypass         = ["AzureServices", "Logging"]
+#  min_tls_version              = "TLS1_2"
+#  log_analytics_workspace_id   = azurerm_log_analytics_workspace.Log_Analytics_Workspace_Default.id
+#  advanced_threat_protection   = "false"
+#}
+
+resource "azurerm_storage_account" "st-diagstore" {
+  name                = "${local.Project}${var.AzureResourceTypes["StorageAccount"]}diagstore${local.Environment}"
+  resource_group_name = azurerm_resource_group.resource-group-virtual-network.name
+
+  location                 = local.Location
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = []
+    bypass                     = ["AzureServices", "Logging"]
+    virtual_network_subnet_ids = []
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
 }
 
 
@@ -28,6 +50,28 @@ module "diagstore" {
 #  advanced_threat_protection   = "false"
 #}
 
+resource "azurerm_storage_account" "st-flowlogstorage" {
+  name                = "${local.Project}${var.AzureResourceTypes["StorageAccount"]}flowlogstorage${local.Environment}"
+  resource_group_name = azurerm_resource_group.resource-group-virtual-network.name
+
+  location                 = local.Location
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = []
+    bypass                     = ["AzureServices", "Logging"]
+    virtual_network_subnet_ids = []
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
+
 #module "k8spvc" {
 #  source                       = "../../../../modules/terraform.azurerm.module.storageaccount"
 #  name                         = "${local.Project}${var.AzureResourceTypes["StorageAccount"]}k8spvc${local.Environment}"
@@ -43,3 +87,26 @@ module "diagstore" {
 #  log_analytics_workspace_id   = azurerm_log_analytics_workspace.Log_Analytics_Workspace_Default.id
 #  advanced_threat_protection   = "false"
 #}
+
+
+resource "azurerm_storage_account" "st-k8spvc" {
+  name                = "${local.Project}${var.AzureResourceTypes["StorageAccount"]}k8spvc${local.Environment}"
+  resource_group_name = azurerm_resource_group.resource-group-virtual-network.name
+
+  location                 = local.Location
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+
+  network_rules {
+    default_action             = "Deny"
+    ip_rules                   = []
+    bypass                     = ["AzureServices", "Logging"]
+    virtual_network_subnet_ids = []
+  }
+
+  tags = {
+    Environment = "Dev"
+  }
+}
