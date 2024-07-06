5ac4badcd413ac969371e96b6ab496e1e043272b
Ky Tran <trantheky1987@gmail.com>
2024-07-06 16:37:31 +0700

    updated

--- stardog/environments/development/infrastructure/z_12_azure_aks.tf
+++ stardog/environments/development/infrastructure/z_12_azure_aks.tf
@@ -64,3 +64,40 @@ resource "azurerm_kubernetes_cluster" "this" {
     depends_on = [ azurerm_role_assignment.aks_managed_identity_role_assignment_aks_rg ]
 
 }
+
+
+
+
+resource "azurerm_kubernetes_cluster_node_pool" "spot" {
+    name                  = "spot"
+    kubernetes_cluster_id = azurerm_kubernetes_cluster.this.id
+    vm_size               = "Standard_DS2_v2"
+    vnet_subnet_id        = azurerm_subnet.subnet1.id
+    orchestrator_version  = "1.28.9"
+    priority              = "Spot"
+    spot_max_price        = -1
+    eviction_policy       = "Delete"
+
+    enable_auto_scaling = true
+    node_count          = 1
+    min_count           = 1
+    max_count           = 10
+
+    node_labels = {
+        role                                    = "spot"
+        "kubernetes.azure.com/scalesetpriority" = "spot"
+    }
+
+    node_taints = [
+        "spot:NoSchedule",
+        "kubernetes.azure.com/scalesetpriority=spot:NoSchedule"
+    ]
+
+    tags = {
+        Environment = "Development"
+    }
+
+    lifecycle {
+        ignore_changes = [node_count]
+    }
+}
